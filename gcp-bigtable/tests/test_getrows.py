6976df5f1d0f6c7f3e1853c62ad24485114d89e0
George T. C. Lai <george@innotech.me>
2020-10-26 16:18:45 +0800

    test(gcp/bigtable): add test corresponding to row key range scan
    add test corresponding to row key range scan

--- gcp-bigtable/tests/test_getrows.py
+++ gcp-bigtable/tests/test_getrows.py
@@ -6,6 +6,7 @@ import pytest
 from typing import List
 from getrows import get_rowkeys
 from getrows import get_table_instance
+from getrows import scan_rows_range
 
 
 class TestGetrows(object):
@@ -26,6 +27,16 @@ class TestGetrows(object):
 
 
     @pytest.fixture(scope="class")
+    def start_rowkey_single_vendor(self):
+        return "1:213:7654321:ou:0:pre:betradar:0"
+    
+    
+    @pytest.fixture(scope="class")
+    def stop_rowkey_single_vendor(self):
+        return "1:213:7654321:ou:0:pre:betradar:z"
+
+
+    @pytest.fixture(scope="class")
     def table_instance(self, projectid, instanceid, tablename):
         table = get_table_instance(projectid, instanceid, tablename)
         return table
@@ -40,4 +51,17 @@ class TestGetrows(object):
     def test_get_multiple_rowkeys(self, table_instance, rowkeys: List[str]):
         assert len(rowkeys) > 1
         result_models = get_rowkeys(table_instance, rowkeys, sep = ":")
+        assert len(result_models) > 1
+    
+    def test_row_scan_single_vendor(
+        self,
+        table_instance,
+        start_rowkey_single_vendor,
+        stop_rowkey_single_vendor
+    ):
+        result_models = scan_rows_range(
+                                        table_instance,
+                                        start_rowkey_single_vendor,
+                                        stop_rowkey_single_vendor,
+                                        sep = ":")
         assert len(result_models) > 1
